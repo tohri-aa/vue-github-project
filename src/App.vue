00fbb01fe6de91c60edf5cda75257e46ae90e55e
Victoria Akeju <akejuvictoria7@gmail.com>
2024-06-08 12:29:20 +0100

    installed vue-router and axios

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,10 @@
 <script setup>
-import Myrepos from './components/repos.vue'
+import Repos from './components/repos.vue'
 </script>
 
 <template>
-  <Myrepos/>
+  <Repos/>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
 </style>
--- src/components/repos.vue
+++ src/components/repos.vue
@@ -1,7 +1,31 @@
 <script>
+    import axios from 'axios'
 
+    export default {
+  data() {
+    return {
+      repos: []
+    };
+  },
+  async created() {
+    try {
+      const response = await axios.get('https://api.github.com/users/tohri-aa/repos');
+      this.repos = response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
 </script>
 
 <template>
     <h1>My Repositories</h1>
+    <ul>
+        <RouterLink to="/" v-for="repo in repos">
+            <h3>
+                {{ repo.name }}
+            </h3>
+            <p>{{ repo.description ? repo.description : "no description"}}</p>
+        </RouterLink>
+    </ul>
 </template>
